a3c8e8d00ee291c574b05891218951b106d164fc
Joshei <joshuaeirm@gmail.com>
2021-03-05 17:42:57 -0500

    products for orders line up

--- lookatorders.php
+++ lookatorders.php
@@ -22,24 +22,15 @@ $dbo0 = new PDO("mysql:host=$host;dbname=$database", $user, $pass, $options);
 
 
 
+
 $string0 =  "
 <br>
 <div class=\"container\">
-<div class = \"row\">
-</div>
-<div class = \"row\">
-</div>
+
+
 <table style=\"width:100%\" border=\"1\" > 
 
-<tr>
 
-<th>Status</th>
-<th>Date</th> 
-<th>ID</th> 
-<th>Product</th>
-<th>Quantity</th>
-<th>Cost</th> 
-</tr>
 
 ";
 
@@ -79,22 +70,17 @@ $pQuant = strval ($pQuant);
 
 
 //in for loop
-//if pID just changed
+//if pID just changed  OR JUST STARTED
 
-if($pID != $lastOrderID && $lastOrderID != "")
+if($pID != $lastOrderID )
 {
   $string0 .=  "</table>
-<br>
-<div class=\"container\">
-<div class = \"row\">
-</div>
-<div class = \"row\">
-</div>
+
+
 
 <table style=\"width:100%\" border=\"1\" > 
-//b
-<tr>
 
+<tr>
 <th>Status</th>
 <th>Date</th> 
 <th>ID</th> 
@@ -103,9 +89,7 @@ if($pID != $lastOrderID && $lastOrderID != "")
 <th>Cost</th> 
 </tr>
 
-//c
-
-
+//ww
 <tr>
 <td>STATUS HERE</td>
 <td>" . $orderDate . " </td>
@@ -114,34 +98,34 @@ if($pID != $lastOrderID && $lastOrderID != "")
 <td>" . $pQuant  . "</td>
 <td>" . $pCost . "</td>
 </tr>
-
-/////////
-
-
-/////////
-
 ";
-  
-  
-  
- 
 }
-//same pid as last pic
+//same pid as last pid
 else
 {
   
  $string0 .= "
 
-  <input type=\"radio\" id=\"$statusA\" name=\"age\" value=\"30\">
-  <label for=\"status1\">Purchased</label>
+</table  > 
+ <input type=\"radio\" id=\"$statusA\" name=\"age\" value=\"30\">
+ <label>      Purchased      </label>
   <input type=\"radio\" id=\"$statusB\" name=\"age\" value=\"60\">
-  <label for=\"status2\">Shipped</label>  
+  <label> Shipped </label>  
   <input type=\"radio\" id=\"$statusC\" name=\"age\" value=\"100\">
-  <label for=\"status3\">Refunded</label>
-  
+  <label>Refunded </label>
+  <table  style=\"width:100%\" border=\"1\" > 
 
-d
-  <tr>
+<tr>
+<th class = \"one\">Status</th>
+<th class = \"one\">Date</th> 
+<th class = \"one\" >ID</th> 
+<th  class = \"one\">Product</th>
+<th class = \"one\">Quantity</th>
+<th class = \"one\">Cost</th> 
+</tr>
+
+
+<tr>
 <td>STATUS HERE</td>
 <td>" . $orderDate . " </td>
 <td>" . $pID . "</td>
@@ -151,14 +135,19 @@ d
 </tr>
 
 </table>
-<br><br>
+
+
+<input type=\"radio\" id=\"$statusA\" name=\"age\" value=\"30\">
+<label >Purchased </label>
+  <input type=\"radio\" id=\"$statusB\" name=\"age\" value=\"60\">
+  <label >Shipped </label>  
+  <input type=\"radio\" id=\"$statusC\" name=\"age\" value=\"100\">
+  <label >Refunded </label>
+
+
 text
 
-<br>
-<br>
-
-
-<table>
+<table  style=\"width:100%\" border=\"1\" >
 ";
 }
 
@@ -167,10 +156,19 @@ text
 
 
 }//for each
+
+
+
+
 $string0 .= "</table>";
 
 
-
+$string0 .= "<input type=\"radio\" id=\"$statusA\" name=\"age\" value=\"30\">
+ <label >Purchased </label>
+  <input type=\"radio\" id=\"$statusB\" name=\"age\" value=\"60\">
+  <label >Shipped </label>  
+  <input type=\"radio\" id=\"$statusC\" name=\"age\" value=\"100\">
+  <label >Refunded </label>";
 
 
 
